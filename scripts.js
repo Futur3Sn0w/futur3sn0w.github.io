ac78a8479f11076bcbec65bde559a0fe73e1a821
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2021-06-08 22:23:58 -0400

    New navigation (again...)

--- scripts.js
+++ scripts.js
@@ -1,53 +1,52 @@
 function aboutBtn() {
-    document.title = "Futur3Sn0w Studio | About Me";
-    document.getElementById("greetingLinks").style.opacity = "0";
-    document.getElementById("psWrapper").style.opacity = "0";
-    document.getElementById("aboutBox").style.opacity = "1";
-    document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
-    document.getElementById("nbHome").innerHTML = " <label>Home</label>";
-    document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
-    document.getElementById("nbAbout").style.font = "25px FLNTFILL";
-    document.getElementById("nbHome").style.font = "25px FLNTREG";
-    document.getElementById("nbProjects").style.font = "25px FLNTREG";
-    document.getElementById("greetingLinks").style.pointerEvents = "none";
-    document.getElementById("psWrapper").style.pointerEvents = "none";
-    document.getElementById("aboutBox").style.pointerEvents = "auto";
+    // document.getElementById("greetingLinks").style.opacity = "0";
+    // document.getElementById("psWrapper").style.opacity = "0";
+    // document.getElementById("aboutBox").style.opacity = "1";
+    // document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
+    // document.getElementById("nbHome").innerHTML = " <label>Home</label>";
+    // document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
+    // document.getElementById("nbAbout").style.font = "25px FLNTFILL";
+    // document.getElementById("nbHome").style.font = "25px FLNTREG";
+    // document.getElementById("nbProjects").style.font = "25px FLNTREG";
+    // document.getElementById("greetingLinks").style.pointerEvents = "none";
+    // document.getElementById("psWrapper").style.pointerEvents = "none";
+    // document.getElementById("aboutBox").style.pointerEvents = "auto";
+
+    window.location.href = 'about.html';
 }
 
 function homeBtn() {
-    document.title = "Futur3Sn0w Studio | Home";
-    document.getElementById("greetingLinks").style.opacity = "1";
-    document.getElementById("psWrapper").style.opacity = "0";
-    document.getElementById("aboutBox").style.opacity = "0";
-    document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
-    document.getElementById("nbHome").innerHTML = " <label>Home</label>";
-    document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
-    document.getElementById("nbAbout").style.font = "25px FLNTREG";
-    document.getElementById("nbHome").style.font = "25px FLNTFILL";
-    document.getElementById("nbProjects").style.font = "25px FLNTREG";
-    document.getElementById("greetingLinks").style.pointerEvents = "auto";
-    document.getElementById("psWrapper").style.pointerEvents = "none";
-    document.getElementById("aboutBox").style.pointerEvents = "none";
+    // document.getElementById("greetingLinks").style.opacity = "1";
+    // document.getElementById("psWrapper").style.opacity = "0";
+    // document.getElementById("aboutBox").style.opacity = "0";
+    // document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
+    // document.getElementById("nbHome").innerHTML = " <label>Home</label>";
+    // document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
+    // document.getElementById("nbAbout").style.font = "25px FLNTREG";
+    // document.getElementById("nbHome").style.font = "25px FLNTFILL";
+    // document.getElementById("nbProjects").style.font = "25px FLNTREG";
+    // document.getElementById("greetingLinks").style.pointerEvents = "auto";
+    // document.getElementById("psWrapper").style.pointerEvents = "none";
+    // document.getElementById("aboutBox").style.pointerEvents = "none";
+
+    window.location.href = 'index.html';
 }
 
 function projectsBtn() {
-    document.title = "Futur3Sn0w Studio | My Projects";
-    document.getElementById("greetingLinks").style.opacity = "0";
-    document.getElementById("psWrapper").style.opacity = "1";
-    document.getElementById("aboutBox").style.opacity = "0";
-    /**/
-    document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
-    document.getElementById("nbHome").innerHTML = " <label>Home</label>";
-    document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
-    /**/
-    document.getElementById("nbAbout").style.font = "25px FLNTREG";
-    document.getElementById("nbHome").style.font = "25px FLNTREG";
-    document.getElementById("nbProjects").style.font = "25px FLNTFILL";
-    /**/
-    document.getElementById("greetingLinks").style.pointerEvents = "none";
-    document.getElementById("psWrapper").style.pointerEvents = "auto";
-    document.getElementById("aboutBox").style.pointerEvents = "none";
-    /**/
+    // document.getElementById("greetingLinks").style.opacity = "0";
+    // document.getElementById("psWrapper").style.opacity = "1";
+    // document.getElementById("aboutBox").style.opacity = "0";
+    // document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
+    // document.getElementById("nbHome").innerHTML = " <label>Home</label>";
+    // document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
+    // document.getElementById("nbAbout").style.font = "25px FLNTREG";
+    // document.getElementById("nbHome").style.font = "25px FLNTREG";
+    // document.getElementById("nbProjects").style.font = "25px FLNTFILL";
+    // document.getElementById("greetingLinks").style.pointerEvents = "none";
+    // document.getElementById("psWrapper").style.pointerEvents = "auto";
+    // document.getElementById("aboutBox").style.pointerEvents = "none";
+
+    window.location.href = 'projects.html';
 }
 
 fetch('projectSums/1.txt')
