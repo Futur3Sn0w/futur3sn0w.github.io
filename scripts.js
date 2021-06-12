1f0567b65ecdadd09388257ba90fbd1c793315f9
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2021-06-11 22:48:37 -0400

    New page

--- scripts.js
+++ scripts.js
@@ -1,51 +1,16 @@
 function aboutBtn() {
-    // document.getElementById("greetingLinks").style.opacity = "0";
-    // document.getElementById("psWrapper").style.opacity = "0";
-    // document.getElementById("aboutBox").style.opacity = "1";
-    // document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
-    // document.getElementById("nbHome").innerHTML = " <label>Home</label>";
-    // document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
-    // document.getElementById("nbAbout").style.font = "25px FLNTFILL";
-    // document.getElementById("nbHome").style.font = "25px FLNTREG";
-    // document.getElementById("nbProjects").style.font = "25px FLNTREG";
-    // document.getElementById("greetingLinks").style.pointerEvents = "none";
-    // document.getElementById("psWrapper").style.pointerEvents = "none";
-    // document.getElementById("aboutBox").style.pointerEvents = "auto";
-
     window.location.href = 'about.html';
 }
 
-function homeBtn() {
-    // document.getElementById("greetingLinks").style.opacity = "1";
-    // document.getElementById("psWrapper").style.opacity = "0";
-    // document.getElementById("aboutBox").style.opacity = "0";
-    // document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
-    // document.getElementById("nbHome").innerHTML = " <label>Home</label>";
-    // document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
-    // document.getElementById("nbAbout").style.font = "25px FLNTREG";
-    // document.getElementById("nbHome").style.font = "25px FLNTFILL";
-    // document.getElementById("nbProjects").style.font = "25px FLNTREG";
-    // document.getElementById("greetingLinks").style.pointerEvents = "auto";
-    // document.getElementById("psWrapper").style.pointerEvents = "none";
-    // document.getElementById("aboutBox").style.pointerEvents = "none";
+function liveFeedBtn() {
+    window.location.href = 'liveFeed.html';
+}
 
+function homeBtn() {
     window.location.href = 'index.html';
 }
 
 function projectsBtn() {
-    // document.getElementById("greetingLinks").style.opacity = "0";
-    // document.getElementById("psWrapper").style.opacity = "1";
-    // document.getElementById("aboutBox").style.opacity = "0";
-    // document.getElementById("nbAbout").innerHTML = " <label>About Me</label>";
-    // document.getElementById("nbHome").innerHTML = " <label>Home</label>";
-    // document.getElementById("nbProjects").innerHTML = " <label>My Projects</label>";
-    // document.getElementById("nbAbout").style.font = "25px FLNTREG";
-    // document.getElementById("nbHome").style.font = "25px FLNTREG";
-    // document.getElementById("nbProjects").style.font = "25px FLNTFILL";
-    // document.getElementById("greetingLinks").style.pointerEvents = "none";
-    // document.getElementById("psWrapper").style.pointerEvents = "auto";
-    // document.getElementById("aboutBox").style.pointerEvents = "none";
-
     window.location.href = 'projects.html';
 }
 
